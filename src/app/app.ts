b0ea94056ffcc5599aa441d6bda94cc68d15d4ed
mgechev <mgechev@gmail.com>
2025-05-19 19:14:23 +0300

    Improve styles

--- src/app/app.ts
+++ src/app/app.ts
@@ -7,30 +7,49 @@ interface Todo {
 
 @Component({
   selector: 'app-root',
+  host: {
+    'click': 'handleClick()'
+  },
   template: `
     <h1>{{ title }}</h1>
 
-    <input #newTodo type="text" placeholder="New todo...">
-    <button (click)="addTodo(newTodo.value); newTodo.value = '';">Add</button>
+    <input #newTodo type="text" placeholder="New todo..." />
+    <button (click)="addTodo(newTodo.value); newTodo.value = ''">Add</button>
     <ul>
-    @for (todo of todos; track todo) {
-      <li>{{ todo.name }}</li>
-    }
+      @for (todo of todos; track todo) {
+      <li [class.completed]="todo.completed">
+        <input
+          (click)="toggleTodo(todo)"
+          [checked]="todo.completed"
+          type="checkbox"
+        />{{ todo.name }}
+        <button (click)="deleteTodo(todo)">Delete</button>
+      </li>
+      }
     </ul>
   `,
-  styleUrl: './app.css'
+  styleUrl: 'app.css'
 })
 export class App {
   title = 'Todo management';
 
   todos: Todo[] = [
     { name: 'Get coffee', completed: true },
-    { name: 'Catch 94', completed: false }
+    { name: 'Catch 94', completed: false },
   ];
 
   addTodo(name: string) {
     this.todos.push({
-      name, completed: false
+      name,
+      completed: false,
     });
+  }
+
+  toggleTodo(todo: Todo) {
+    todo.completed = !todo.completed;
+  }
+
+  deleteTodo(todo: Todo) {
+    this.todos.splice(this.todos.indexOf(todo), 1);
   }
 }
